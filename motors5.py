db4a01f6adcbded3aaf13040071cb39cbfb0c990
alexreid257 <alexreid257@GMAIL.COM>
2015-04-08 20:13:11 -0400

    Tippens-proofing motors

--- motors5.py
+++ motors5.py
@@ -45,7 +45,7 @@ def motors_init():
     PWM.start(Motor2A, duty_forward, 60.0, 1)
     GPIO.setup(Motor1B, GPIO.OUT)		# Sets up pins as GPIO outputs
     GPIO.setup(Motor2B, GPIO.OUT)
-print "Ready"
+    print "Ready"
 
 
 # ==============================================================
@@ -62,9 +62,8 @@ def motors_forward(d):
     GPIO.output(Motor1B, GPIO.LOW)  # GPIO LOW is forward
     PWM.set_duty_cycle(Motor2A, d)
     GPIO.output(Motor2B, GPIO.LOW)
+    print "Forward"
     return d
-print "Forward"
-
 
 # ==============================================================
 # Function:  motors_reverse
@@ -80,41 +79,50 @@ def motors_reverse():
     GPIO.output(Motor1B, GPIO.HIGH)
     PWM.stop(Motor2A)
     GPIO.output(Motor2B, GPIO.HIGH)
-print "Reverse"
+    print "Reverse"
 
 
 # ==============================================================
-# Motors right
+# Function:  motors_right
+# Returns:   None
+# Arguments: None
+# Summary:   Sets duty cycle to the same value for both motors,
+#            but in opposite directions to accomplish a zero-
+#            degree right turn.
 # ==============================================================
 
-
-def motors_right(d):
+def motors_right():
     PWM.set_duty_cycle(Motor1A, 7.5)
     GPIO.output(Motor1B, GPIO.HIGH)
-    PWM.set_duty_cycle(Motor2A, d)
+    PWM.set_duty_cycle(Motor2A, 7.5)
     GPIO.output(Motor2B, GPIO.LOW)
     PWM.stop(Motor1A)
     PWM.stop(Motor2A)
-    return d
-    
-print "Right"
+    print "Right"
 
 
 # ==============================================================
-# Motors left
+# Function:  motors_left
+# Returns:   None
+# Arguments: None
+# Summary:   Sets duty cycle to the same value for both motors,
+#            but in opposite directions to accomplish a zero-
+#            degree left turn.
 # ==============================================================
 
 
-def motors_left(d):
-    PWM.set_duty_cycle(Motor1A, d)
+def motors_left():
+    PWM.set_duty_cycle(Motor1A, 7.5)
     GPIO.output(Motor1B, GPIO.LOW)
-    PWM.set_duty_cycle(Motor2A, d)
+    PWM.set_duty_cycle(Motor2A, 7.5)
     GPIO.output(Motor2B, GPIO.HIGH)
-    return d
-print "Left"
+    print "Left"
 
 # ==============================================================
-# Motors Stop
+# Function:  motors_stop
+# Returns:   None
+# Arguments: None
+# Summary:   Stops motors
 # ==============================================================
 
 
@@ -126,16 +134,11 @@ def motors_stop():
 print "Stop"
 
 # ==============================================================
-# Motors kill
-# ==============================================================
-
-def motors__kill():
-    PWM.stop(Motor1A)
-    PWM.stop(Motor2A)
-print "Signals Killed"
-
-# ==============================================================
-# Motors right control
+# Function:  motors_right_control
+# Returns:   None
+# Arguments: dut (Duty cycle), direction)
+# Summary:   Direct control of right motor, in case a program
+#            accessing this library needs to do so
 # ==============================================================
 
 
@@ -150,7 +153,11 @@ def motors_right_control(dut, direction):
         print "Direction error"
 
 # ==============================================================
-# Motors left control
+# Function:  motors_left_control
+# Returns:   None
+# Arguments: dut (Duty cycle), direction)
+# Summary:   Direct control of left motor, in case a program
+#            accessing this library needs to do so
 # ==============================================================
 
 
@@ -165,8 +172,13 @@ def motors_left_control(dut, direction):
         print "Direction Error"
 
 # ==============================================================
-# Motors Cleanup
+# Function:  motors_cleanup
+# Returns:   None
+# Arguments: None
+# Summary:   Stops all control of motors and servos
 # ==============================================================
+
+
 def motors_cleanup():
     GPIO.cleanup()  # Kills all GPIO signals
     PWM.cleanup() 	# Kills all PWM signals
